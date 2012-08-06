3201bf6e3d30c19b3824e61b793f171015673520
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-08-06 18:59:14 +0200

    Update license notices and add GPL 3 text.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,18 +1,23 @@
 /****************************************************************************
+ * This file is part of Terminal
  *
- * Copyright (c) 2010 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ * Copyright (c) 2010-2012 Pier Luigi Fiorini
  *
- * All rights reserved.
- * Contact: Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
  *
- * GNU General Public License Usage
- * This file may be used under the terms of the GNU General Public
- * License version 2 as published by the Free Software Foundation
- * and appearing in the file LICENSE.GPL included in the packaging
- * of this file.  Please review the following information to
- * ensure the GNU General Public License version 2 requirements
- * will be met: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
+ * Terminal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * Terminal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Terminal.  If not, see <http://www.gnu.org/licenses/>.
  ***************************************************************************/
 
 #include "mainwindow.h"
