8db3de1d14c3b57d78a7137fa2a5ffcd1399300e
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-10-27 09:51:56 +0200

    Fix typo.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -29,7 +29,7 @@
 #include "lib/qtermwidget.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent),
+    : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
