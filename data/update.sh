cc938bf8d414cf63ac351768d2b35ca58d54e02a
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2013-09-15 20:14:49 +0200

    Replace update.sh with a better script
    Write a new update script in Python which produces a far better output
    and discards translations for the Icon entry.

--- data/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-basefilename=hawaii-terminal
-cat ${basefilename}.desktop.in > ${basefilename}.desktop
-grep --no-filename '\[.*]\s*=' translations/${basefilename}_*.desktop >> ${basefilename}.desktop
