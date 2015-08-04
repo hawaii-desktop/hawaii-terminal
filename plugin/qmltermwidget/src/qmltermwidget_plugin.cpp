2e39e9077ad4735b4bd47c341b0eca22d5e8c6a7
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-08-04 22:58:08 +0200

    Fix path for keyboard layout and color schemes

--- plugin/qmltermwidget/src/qmltermwidget_plugin.cpp
+++ plugin/qmltermwidget/src/qmltermwidget_plugin.cpp
@@ -27,8 +27,8 @@ void QmltermwidgetPlugin::initializeEngine(QQmlEngine *engine, const char *uri)
         QString cs, kbl;
 
         foreach (QString pwd, pwds) {
-            cs  = pwd + "/QMLTermWidget/color-schemes";
-            kbl = pwd + "/QMLTermWidget/kb-layouts";
+            cs  = pwd + "/Hawaii/Terminal/QMLTermWidget/color-schemes";
+            kbl = pwd + "/Hawaii/Terminal/QMLTermWidget/kb-layouts";
             if (QDir(cs).exists()) break;
         }
 
